ef04baab09cf758cff591fa5d671ac9be937de7a
ikoshura <abrar.zharif@gmail.com>
2025-08-28 19:39:34 +0700

    Initial Commit

---
+++ Sajda/SajdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SajdaApp.swift
+//  Sajda
+//
+//  Created by Aliyya Nazhifah on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SajdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
